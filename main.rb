90849110fa36794161690feee4cb67e467b34d03
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-10-22 17:39:29 -0300

    Add my_each method

--- main.rb
+++ main.rb
@@ -1,1 +1,11 @@
-puts "Hello World"
+module Enumerable
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    count = 0
+    while count < to_a.length
+      yield to_a[count]
+      count += 1
+    end
+  end
+end
