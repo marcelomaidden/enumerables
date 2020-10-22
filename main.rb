172ff91699091391a67f5052c9e4973a11be09f5
Juan Raudales <juanraudales@unitec.edu>
2020-10-22 14:51:22 -0600

    add my_each_with_index def

--- main.rb
+++ main.rb
@@ -8,4 +8,14 @@ module Enumerable
       count += 1
     end
   end
+
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
+    count = 0
+    while count < to_a.length
+      yield(to_a[count], count)
+      count += 1
+    end
+  end
 end
